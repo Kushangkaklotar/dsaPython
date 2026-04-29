08ec5d2233e149d351946d2b59576305c364c7cb
Kushangkaklotar <133807054+Kushangkaklotar@users.noreply.github.com>
2026-04-29 23:29:23 +0530

    change in bug

--- 1.two_sum.py
+++ 1.two_sum.py
@@ -1,12 +1,11 @@
 
 class Solution:
     def twoSum(nums: [int], target: int) -> [int]:
-
         for i in range(len(nums)):
-            if i >= range(len(nums)):
+            if i <= len(nums):
                 if nums[i] + nums[i+1] == target:
-                    return[nums[i], nums[i+1]]
+                    return[i, i+1]
         
         return nums
 
-print(Solution.twoSum(nums=[2,7,11,15], target=9))
+print(Solution.twoSum(nums=[3,2,3], target=6))
